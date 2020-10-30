0f9393cc0da0351f632789e0d96eb77a66f5eb1a
Rodolfo Inostroza <ralphweb@gmail.com>
2020-10-30 17:30:28 -0300

    terminado

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,77 @@
 <template>
-  <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
-  </div>
+<div class="home">
+    <div class="personaje" v-for="(personaje,indice) in data" v-bind:key="indice">
+        <div class="personaje-foto" :style="'background-image:url('+personaje.image+');'">
+        </div>
+        <div class="personaje-nombre">
+            {{personaje.name}}
+        </div>
+    </div>
+</div>
 </template>
 
 <script>
-// @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
-
 export default {
-  name: 'Home',
-  components: {
-    HelloWorld
-  }
+    name: 'Home',
+    components: {
+
+    },
+    data() {
+        return {
+            data: [{
+                    name: 'Rick Sanchez',
+                    image: 'https://rickandmortyapi.com/api/character/avatar/1.jpeg'
+                },
+                {
+                    name: 'Morty Smith',
+                    image: 'https://rickandmortyapi.com/api/character/avatar/2.jpeg'
+                },
+                {
+                    name: 'Summer Smith',
+                    image: 'https://rickandmortyapi.com/api/character/avatar/3.jpeg'
+                },
+                {
+                    name: 'Beth Smith',
+                    image: 'https://rickandmortyapi.com/api/character/avatar/4.jpeg'
+                },
+                {
+                    name: 'Jerry Smith',
+                    image: 'https://rickandmortyapi.com/api/character/avatar/5.jpeg'
+                },
+            ]
+        }
+    },
+    methods: {
+
+    }
 }
 </script>
+
+<style lang="scss">
+.home {
+    background-image: url('/fondo.png');
+    height: 100vh;
+
+    .personaje {
+      width: 100%;
+      box-sizing: border-box;
+      padding: 6px;
+      display: flex;
+      align-items: center;
+
+      &-foto  {
+        width: 120px;
+        height: 120px;
+        border-radius: 120px;
+        background-size: cover;
+        background-position: center;
+      }
+      &-nombre {
+        color: #1b8000;
+        font-weight: 600;
+        font-size: 25px;
+        padding: 10px;
+      }
+    }
+}
+</style>
